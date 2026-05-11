8899f088b5cb9485944eb4c1a332aa1de912753a
郑江江 <67499006+zhengjiangjiang0724@users.noreply.github.com>
2026-05-11 14:53:38 +0800

    fix: e2e test expects 2 replicas not 1
    The deployment has 2 replicas for leader election testing.
    Change test to accept any number >= 1 instead of exactly 1.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -177,7 +177,7 @@ var _ = Describe("Manager", Ordered, func() {
 				podOutput, err := utils.Run(cmd)
 				g.Expect(err).NotTo(HaveOccurred(), "Failed to retrieve controller-manager pod information")
 				podNames := utils.GetNonEmptyLines(podOutput)
-				g.Expect(podNames).To(HaveLen(1), "expected 1 controller pod running")
+				g.Expect(podNames).NotTo(BeEmpty(), "expected at least 1 controller pod running")
 				controllerPodName = podNames[0]
 				g.Expect(controllerPodName).To(ContainSubstring("controller-manager"))
 
